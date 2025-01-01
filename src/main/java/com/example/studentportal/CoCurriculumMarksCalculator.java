bd753f1c475c7685a1ec4df6b27806bfc56f09c0
kimhongzhang323 <kim.hong.zhang323@gmail.com>
2025-01-01 14:42:29 +0800

    Remove password

--- src/main/java/com/example/studentportal/CoCurriculumMarksCalculator.java
+++ src/main/java/com/example/studentportal/CoCurriculumMarksCalculator.java
@@ -15,7 +15,7 @@ public class CoCurriculumMarksCalculator {
     // Database connection details (URL, username, and password)
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/student_info";
     private static final String USER = "root";
-    private static final String PASSWORD = "buku3b3B";
+    private static final String PASSWORD = "";
 
     // Main entry point of the program
     public static void main(String[] args) throws Exception {
